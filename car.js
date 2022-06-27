4b13957cd1015733b0f2ddfe0cd25bbf300e4638
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-06-27 16:52:02 +0200

    Exercise 3 added

--- car.js
+++ car.js
@@ -42,95 +42,113 @@
 
 //02 - TV
 
-class TV {
-  constructor (brand, channel, volume) {
-    this.brand = brand
-    this.channel = 1
-    this.volume = 50
-  }
+// class TV {
+//   constructor (brand, channel, volume) {
+//     this.brand = brand
+//     this.channel = 1
+//     this.volume = 50
+//   }
 
-  volumeUp = (up) => {
-    if (this.volume + up < 100){
-    this.volume = this.volume + up
-    } else {
-    this.volume = 100
-    }
-  }
+//   volumeUp = (up) => {
+//     if (this.volume + up < 100){
+//     this.volume = this.volume + up
+//     } else {
+//     this.volume = 100
+//     }
+//   }
 
-  volumeDown = (down) => {
-    if (this.volume - down > 0){
-    this.volume = this.volume - down
-    } else {
-      this.volume = 0
-    }
-  }
+//   volumeDown = (down) => {
+//     if (this.volume - down > 0){
+//     this.volume = this.volume - down
+//     } else {
+//       this.volume = 0
+//     }
+//   }
 
-  channelUp = (up) => {
-    if (this.chanel + up >50){
-      this.chanel = this.chanel
-    } else{
-      this.channel = this.channel + up
-    }
-  }
+//   channelUp = (up) => {
+//     if (this.chanel + up >50){
+//       this.chanel = this.chanel
+//     } else{
+//       this.channel = this.channel + up
+//     }
+//   }
 
-  channelDown = (down) => {
-    if (this.channel - down < 1){
-      this.channel = this.channel
-    } else {
-      this.channel = this.channel - down
-    }
-  }
+//   channelDown = (down) => {
+//     if (this.channel - down < 1){
+//       this.channel = this.channel
+//     } else {
+//       this.channel = this.channel - down
+//     }
+//   }
 
-  reset = () => {
-    this.channel = 1
-    this.volume = 50
-  }
+//   reset = () => {
+//     this.channel = 1
+//     this.volume = 50
+//   }
 
-  describe = () =>{
-    console.log(`Your ${this.brand} TV is on channel ${this.channel} and the volume level is ${this.volume}`)
-  }
+//   describe = () =>{
+//     console.log(`Your ${this.brand} TV is on channel ${this.channel} and the volume level is ${this.volume}`)
+//   }
 
-}
+// }
 
-panasonic = new TV ("Panasonic", this.channel , this.volume)
+// panasonic = new TV ("Panasonic", this.channel , this.volume)
 // console.log(panasonic)
 
-panasonic.volumeUp(52)
+// panasonic.volumeUp(52)
 // console.log(panasonic)
 
-panasonic.volumeDown(30)
-// console.log(panasonic)
-
-
-panasonic.channelDown(53)
-// console.log(panasonic)
-
-panasonic.reset()
-// console.log(panasonic)
-
-panasonic.channelUp(33)
-// console.log(panasonic)
-
-panasonic.volumeUp(22)
+// panasonic.volumeDown(30)
 // console.log(panasonic)
 
 
-panasonic.describe()
+// panasonic.channelDown(53)
 // console.log(panasonic)
 
-sony = new TV ("Sony", this.channel, this.volume)
+// panasonic.reset()
+// console.log(panasonic)
 
-sony.volumeDown(23)
-sony.channelUp(13)
-sony.describe()
+// panasonic.channelUp(33)
+// console.log(panasonic)
+
+// panasonic.volumeUp(22)
+// console.log(panasonic)
+
+
+// panasonic.describe()
+// console.log(panasonic)
+
+// sony = new TV ("Sony", this.channel, this.volume)
+
+// sony.volumeDown(23)
+// sony.channelUp(13)
+// sony.describe()
 
 
 //03 - Video games
 
-// class Characters {
-//   constructor (name, attack, defense, series) {
-//     this.name = null
-//     this.attack = 0
-//     this.defense = 0
-//     this.series = null  }
-// }
+//chosen game: Street Fighter 2
+
+class GameModes {
+  constructor (numberOfPlayers, numberOfStages) {
+    this.name = null
+    this.numberOfPlayers = 1
+    this.numberOfStages = 1
+  }
+}
+
+class Characters {
+  constructor (attack, defense) {
+    this.name = null
+    this.specialMoves = null
+    this.attack = 0
+    this.defense = 0
+  }
+}
+
+class Stages {
+  constructor (size) {
+    this.name = null
+    this.size = large
+  }
+}
